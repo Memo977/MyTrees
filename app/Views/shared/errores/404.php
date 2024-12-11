919575161e1712a51a748fed1b4455bee516ee08
Memo977 <gsolorzanoo@est.utn.ac.cr>
2024-12-11 07:41:07 -0600

    Retoque final

--- app/Views/shared/errores/404.php
+++ app/Views/shared/errores/404.php
@@ -9,7 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet">
-    <link href="<?= base_url('assets/css/modules/_404.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/404.css') ?>" rel="stylesheet">
 </head>
 
 <body>
